73431a544b89049eea882b6f731448e9304bd39e
blovus <974184797@qq.com>
2019-05-04 00:44:25 +0800

    添加了“其它模块”用来放置一些特殊代码。在其中添加了mbg代码生成工具。

---
+++ bv-mall-parent/bv-mall-other/bv-mall-other-mbg/src/main/java/com/bv/mbg/CommentPlugin.java
@@ -0,0 +1,119 @@
+package com.bv.mbg;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.internal.util.StringUtility;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @ProjectName: test
+ * @Package: com.test
+ * @ClassName: CommentPlugin
+ * @Author: blovus
+ * @Description: 自定义mbg插件
+ * @Date: 2019/5/3 20:04
+ * @Version: 1.0
+ */
+public class CommentPlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+
+        //添加domain的import
+        topLevelClass.addImportedType("lombok.Data");
+        topLevelClass.addImportedType("lombok.Builder");
+        topLevelClass.addImportedType("lombok.NoArgsConstructor");
+        topLevelClass.addImportedType("lombok.AllArgsConstructor");
+        topLevelClass.addImportedType("io.swagger.annotations.ApiModelProperty");
+        topLevelClass.addImportedType("io.swagger.annotations.ApiModel");
+
+        //添加domain的注解
+        topLevelClass.addAnnotation("@Data");
+        topLevelClass.addAnnotation("@Builder");
+        topLevelClass.addAnnotation("@NoArgsConstructor");
+        topLevelClass.addAnnotation("@AllArgsConstructor");
+        String remarks = introspectedTable.getRemarks();
+
+        if (StringUtility.stringHasValue(remarks)) {
+//            addFieldJavaDoc(field, remarks);
+            //数据库中特殊字符需要转义
+            if (remarks.contains("\"")) {
+                remarks = remarks.replace("\"", "'");
+            }//给model的类添加swagger注解
+            topLevelClass.addAnnotation("@ApiModel(value = \"" + remarks + "\")");
+        }
+
+        //添加domain的注释
+        topLevelClass.addJavaDocLine("/**");
+        topLevelClass.addJavaDocLine("* Created by Mybatis com.bv.mbg.Generator on " + date2Str(new Date()));
+        topLevelClass.addJavaDocLine("*/");
+        return true;
+    }
+
+    @Override
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        String remarks = introspectedColumn.getRemarks();
+        //根据参数和备注信息判断是否添加备注信息
+        if (StringUtility.stringHasValue(remarks)) {
+//            addFieldJavaDoc(field, remarks);
+            //数据库中特殊字符需要转义
+            if (remarks.contains("\"")) {
+                remarks = remarks.replace("\"", "'");
+            }
+            //给model的字段添加swagger注解
+            field.addAnnotation("@ApiModelProperty(value = \"" + remarks + "\")");
+            //文档注释开始
+            field.addJavaDocLine("/**");
+            //获取数据库字段的备注信息
+            String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+            for (String remarkLine : remarkLines) {
+                field.addJavaDocLine(" * " + remarkLine);
+            }
+            field.addJavaDocLine(" */");
+        }
+        return true;
+    }
+
+
+    @Override
+    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        //Mapper文件的注释
+        interfaze.addJavaDocLine("/**");
+        interfaze.addJavaDocLine("* Created by Mybatis com.bv.mbg.Generator on " + date2Str(new Date()));
+        interfaze.addJavaDocLine("*/");
+        return true;
+    }
+
+    @Override
+    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        //不生成getter
+        return false;
+    }
+
+
+    @Override
+    public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        //不生成setter
+        return false;
+    }
+
+    private String date2Str(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        return sdf.format(date);
+    }
+
+}
+
